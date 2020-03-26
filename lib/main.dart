a9f30c93a10fddcf9c7ccbfe92b59f29854125b3
Parth Krishna <thenerdsuperuser@gmail.com>
2020-03-26 20:49:06 +0530

    created a StatelessWidget for the Home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    backgroundColor: Colors.grey[700],
-   appBar: AppBar(
-     title: Text('thenerdsuperuser'),
-     centerTitle: true,
-     backgroundColor: Colors.grey[800],
-   ),
-    body: Center(
-      child: Text(
-        'myapp',
-        style: TextStyle(
-          fontSize: 20.0,
-          color: Colors.white30,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 1.5,
-          fontFamily: 'IndieFlower',
+  home: Home(),
+));
+
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[700],
+      appBar: AppBar(
+        title: Text('thenerdsuperuser'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[800],
+      ),
+      body: Center(
+        child: Text(
+          'My App',
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.white30,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            fontFamily: 'IndieFlower',
+          ),
         ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('click'),
-      onPressed: () {},
-      backgroundColor: Colors.lightGreen[900],
-    ),
-  ),
-));
+      floatingActionButton: FloatingActionButton(
+        child: Text('Hit !'),
+        onPressed: () {},
+        backgroundColor: Colors.lightGreen[800],
+      ),
+    );
+  }
+}
